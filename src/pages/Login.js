69fa06854f544970165fb120d0f4f69a4a2d055c
VanehSann <vanessacbsantos22@gmail.com>
2022-03-31 23:31:43 -0300

    inicinando o projeto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <fieldset>
+        <h1>TybeWallet</h1>
+        <input type="email" data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
+        <button type="button">Entrar</button>
+      </fieldset>
+    );
   }
 }
 
