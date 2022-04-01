f5fdc3741def5c7e6b9c5671498f1970b3574804
VanehSann <vanessacbsantos22@gmail.com>
2022-04-01 11:23:29 -0300

    removi o BrowserRouter, dica do Arthur Procópio, Kauê Alves, Marcos Danilo e Débora Serra.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return (<header>
-      <div>TrybeWallet</div>
-      <p data-testid="email-field">
-        {' '}
-        {}
-        {' '}
-      </p>
-      <span data-testid="total-field">0</span>
-      <span data-testid="header-currency-field">BRL</span>
-    </header>);
+    return (
+      <header>
+        <div>TrybeWallet</div>
+        <p data-testid="email-field">
+          {' '}
+          {}
+          {' '}
+        </p>
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    );
   }
 }
 
